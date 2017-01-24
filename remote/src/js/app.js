38c3a3753b4698bfdbefe564a7cf2a19824518a9
jefBinomed <jean.francois.garreau@gmail.com>
2017-01-24 23:48:56 +0100

    start writing application with polymer 2.0

--- remote/src/js/app.js
+++ remote/src/js/app.js
@@ -7,15 +7,7 @@
 (function (){
 
 	function pageLoad(){
-		document.querySelector(".mdl-button--notifications").addEventListener("click", _ => {
-			document.querySelector(".mdl-layout__drawer-right").classList.add("active");
-			document.querySelector(".mdl-layout__obfuscator-right").classList.add("ob-active");
-		});
-
-		document.querySelector(".mdl-layout__obfuscator-right").addEventListener("click",  _ => {
-			document.querySelector(".mdl-layout__drawer-right").classList.remove("active");
-			document.querySelector(".mdl-layout__obfuscator-right").classList.remove("ob-active");
-		});
+		
 	}
 
 	window.addEventListener('load', pageLoad);
