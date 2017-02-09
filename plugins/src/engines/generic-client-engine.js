e1cc8f6b497c45bb3bff0064fa816d5f9945dcad
jefBinomed <jean.francois.garreau@gmail.com>
2017-02-10 00:39:45 +0100

    add mecanism to communicate with iframe child throught messages

--- plugins/src/engines/generic-client-engine.js
+++ plugins/src/engines/generic-client-engine.js
@@ -3,8 +3,19 @@
 export class GenericEngine{
 
 	constructor(){
-
+		
+		// Listen from instruction comming from remote
+		window.addEventListener("message", this.receiveMessageFromRemote.bind(this), false);		
 	}
+
+	receiveMessageFromRemote(message){
+		if( message.data.charAt( 0 ) === '{' && message.data.charAt( message.data.length - 1 ) === '}' ) {
+			message = JSON.parse( message.data );
+			this.forwardMessageFromRemote(message);
+		}
+	}
+
+	forwardMessageFromRemote(message){}
 
 	goToSlide(params){
 
