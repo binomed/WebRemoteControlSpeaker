e1555bf5bb39549639408a89df3584612ca03093
Jean-Francois Garreau <jean.francois.garreau@gmail.com>
2017-07-20 10:07:07 +0200

    Continue implementing socket messaging

--- src/event-bus/event-bus.js
+++ src/event-bus/event-bus.js
@@ -1,8 +1,42 @@
 class EventBus{
-	constructor(){}
+	constructor(){
+		this.callBacks = {};
+	}
 
-	on(key, callback){}
-	emit(key, obj){}
+	on(key, callback){
+		if (!key){
+			console.warn('Key empty');
+			return;
+		}
+		let arrayCallback = this.callBacks[key];
+		if(!arrayCallback){
+			arrayCallback =[];
+			this.callBacks[key] = arrayCallback;
+		}
+		arrayCallback.push(callback);
+	}
+
+	emit(key, data){
+		if (!key){
+			console.warn('Key empty');
+			return;
+		}
+		const callbacks = this.callBacks[key];
+		if (!callbacks){
+			console.warn(`No call back for key : ${key}`);
+			return;
+		}
+
+		callbacks.forEach((callback) =>{
+			setTimeout(() =>{
+				try{
+					callback(data);
+				}catch(e){
+					console.error(e);
+				}
+			},0);
+		});
+	}
 }
 
 module.exports.EventBus = EventBus;
--- src/event-bus/webscokets/event-bus-websokets.js
+++ src/event-bus/webscokets/event-bus-websokets.js
@@ -4,26 +4,36 @@ const socketIo = require('socket.io');
 class SocketEventBus extends EventBus{
 
 	constructor(server){
+		super();
 		this.io = socketIO(server);
 		this.io.on('connection', (socket) => {
 			console.log('### connection');
+			this.sockets.push(socket);
 			socket.on('message', function(message) {
 				console.log('### message: ' + message);
 				socket.broadcast.emit('message', message);
 			});
 		});
 
-		this.sockets = {};
-		this.callBacks = {};
+		this.sockets = [];
 	}
 
 	on(key, callback){
-		let arrayCallback = this.callBacks[key];
-		if(!arrayCallback){
-			arrayCallback =[];
-			this.callBacks[key] = arrayCallback;
+		super(key, callback);
+		if(!key){
+			return;
 		}
-		arrayCallback.push(callback);
+		this.sockets.forEach((socket) =>{
+			socket.on(key, (message) => {
+				this.emit(key, message);
+			});
+		});
 	}
+
+	emit(key, data){
+		super(key,data);
+		this.io.emit(key, data);
+	}
+
 }
 
